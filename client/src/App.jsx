e50cc6f4e2b5cd431176e5adf5a6c3e3ed5e7036
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2024-11-05 20:31:37 +1100

    basic chakra styling added. still needs to be updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,51 @@
+// import { Outlet } from 'react-router-dom';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+// import Nav from './components/Nav';
+// import { StoreProvider } from './utils/GlobalState';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <div>
+//         <StoreProvider>
+//           <Nav />
+//           <Outlet />
+//         </StoreProvider>
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react"
+import TestChakra from '../src/TestChakra';
 import { Outlet } from 'react-router-dom';
 import {
   ApolloClient,
@@ -6,6 +54,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+// Import ChakraProvider
 
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
@@ -29,15 +78,28 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        heading: { value: `'Figtree', sans-serif` },
+        body: { value: `'Figtree', sans-serif` },
+      },
+    },
+  },
+})
+
+
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>
+      <ChakraProvider value={system}> 
         <StoreProvider>
           <Nav />
           <Outlet />
+          <TestChakra />
         </StoreProvider>
-      </div>
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
---
+++ client/src/TestChakra.jsx
@@ -0,0 +1,14 @@
+// TestChakra.js
+import React from 'react';
+
+// const theme = extendTheme({ colors: { primary: '#ff0000' } });
+
+function TestChakra() {
+  return (
+    // <ChakraProvider theme={theme}>
+      <div style={{ color: 'primary.500' }}>Test Chakra Setup</div>
+    // </ChakraProvider>
+  );
+}
+
+export default TestChakra;
