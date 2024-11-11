ea6df23b100f8e2dc8883814ddcb4bb4261853aa
Alana Hearne <ajhearne@live.com>
2024-11-11 19:44:25 +1100

    database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/community-crate');
 
 module.exports = mongoose.connection;
